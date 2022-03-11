82b00b83f3170a3042c5852cb37e6e139a846c14
ab0136 <aapo.vanhainen@gmail.com>
2022-03-11 09:03:09 +0200

    start screen setup

--- ConBITi Game by fat duck productions/Assets/Aapo/StartMenu.cs
+++ ConBITi Game by fat duck productions/Assets/Aapo/StartMenu.cs
@@ -8,12 +8,42 @@ public class StartMenu : MonoBehaviour
 {
     public GameObject thiscanvas;
     public GameObject creditcanvas;
-
-
+    public GameObject productioncanvas;
+    public GameObject production1;
+    public GameObject production2;
+    public GameObject production3;
+    public GameObject production4;
+    public AudioSource audiosource;
     private void Awake()
     {
         creditcanvas.SetActive(false);
+ 
+
     }
+
+    public IEnumerator Intro()
+    {
+        Debug.Log("wait");
+        production1.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        production2.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        production3.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        production4.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        productioncanvas.SetActive(false);
+        audiosource.enabled = true;
+        
+    }
+
+    private void Start()
+    {
+        StartCoroutine("Intro");
+    }
+
+
+  
 
     public void Play()
     {
